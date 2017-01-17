dc9ddfb03ee95ebffaa52f9e8c939c0df228e9cc
fvrolijken <friso.vrolijken@qiy.nl>
2017-01-17 16:31:51 +0100

    allowing old config for a little bit more

--- src/main/java/nl/qiy/oic/op/qiy/QiyNodeConfig.java
+++ src/main/java/nl/qiy/oic/op/qiy/QiyNodeConfig.java
@@ -20,9 +20,15 @@
 package nl.qiy.oic.op.qiy;
 
 import java.io.File;
+import java.io.FileInputStream;
 import java.io.IOException;
+import java.io.InputStream;
 import java.security.GeneralSecurityException;
 import java.security.KeyFactory;
+import java.security.KeyStore;
+import java.security.KeyStore.PrivateKeyEntry;
+import java.security.KeyStore.ProtectionParameter;
+import java.security.KeyStore.SecretKeyEntry;
 import java.security.PrivateKey;
 import java.security.PublicKey;
 import java.security.spec.PKCS8EncodedKeySpec;
@@ -39,6 +45,8 @@ import org.slf4j.LoggerFactory;
 import com.fasterxml.jackson.annotation.JsonCreator;
 import com.fasterxml.jackson.annotation.JsonProperty;
 import com.google.common.base.Preconditions;
+import com.google.common.base.Strings;
+import com.google.common.base.Throwables;
 
 import io.dropwizard.jackson.Jackson;
 
@@ -66,26 +74,64 @@ public final class QiyNodeConfig {
     // @formatter:off
     public QiyNodeConfig(@JsonProperty("id") String id, 
             @JsonProperty("endpoint") String endpoint,
-            @JsonProperty("secretsFilename") String secretsFilename) throws IOException, GeneralSecurityException { // @formatter:on
+            @JsonProperty("secretsFilename") String secretsFilename, 
+            @JsonProperty("keystore") String keystore, 
+            @JsonProperty("keystorePassPhrase") String keystorePassPhrase, 
+            @JsonProperty("keyPassPhrase") String keyPassPhrase) throws IOException, GeneralSecurityException { // @formatter:on
         super();
         this.id = id;
         this.endpoint = endpoint;
 
-        File secretsFile = new File(secretsFilename);
-        LOGGER.info("Using secrets file " + secretsFile.getAbsolutePath());
-        Map<String, String> secrets = Jackson.newObjectMapper().readValue(secretsFile, Map.class);
-        Preconditions.checkState(this.id.equals(secrets.get("id")),
-                "The id in the config file must match the id in the secrets file");
-        LOGGER.debug("ids match!");
-        KeyFactory keyFactory = KeyFactory.getInstance("RSA");
-        byte[] keyBytes = Base64.getDecoder().decode(secrets.get("privateKey"));
-        this.privateKey = keyFactory.generatePrivate(new PKCS8EncodedKeySpec(keyBytes));
-        LOGGER.debug("private key set");
-        keyBytes = Base64.getDecoder().decode(secrets.get("publicKey"));
-        this.publicKey = keyFactory.generatePublic(new X509EncodedKeySpec(keyBytes));
-        LOGGER.debug("public key set");
-        this.password = secrets.get("nodePassword");
-        LOGGER.debug("password set {}", this.password != null);
+        if (Strings.isNullOrEmpty(secretsFilename)) {
+            LOGGER.info("No secretsFilename found, using keystore (old way)");
+            KeyStore.SecretKeyEntry passwordEntry = (SecretKeyEntry) loadKey("nodesecret", keystore,
+                    "jceks", keystorePassPhrase.toCharArray(), keyPassPhrase.toCharArray());
+            KeyStore.PrivateKeyEntry privateKeyEntry = (PrivateKeyEntry) loadKey("nodekeypair",
+                    keystore, "jceks", keystorePassPhrase.toCharArray(), keyPassPhrase.toCharArray());
+
+            this.privateKey = privateKeyEntry.getPrivateKey();
+            LOGGER.debug("privatekey found");
+            this.publicKey = privateKeyEntry.getCertificate().getPublicKey();
+            LOGGER.debug("public key found");
+            this.password = Base64.getEncoder().encodeToString(passwordEntry.getSecretKey().getEncoded());
+            LOGGER.debug("password found");
+        } else {
+            File secretsFile = new File(secretsFilename);
+            LOGGER.info("Using secrets file " + secretsFile.getAbsolutePath());
+            Map<String, String> secrets = Jackson.newObjectMapper().readValue(secretsFile, Map.class);
+            Preconditions.checkState(this.id.equals(secrets.get("id")),
+                    "The id in the config file must match the id in the secrets file");
+            LOGGER.debug("ids match!");
+            KeyFactory keyFactory = KeyFactory.getInstance("RSA");
+            byte[] keyBytes = Base64.getDecoder().decode(secrets.get("privateKey"));
+            this.privateKey = keyFactory.generatePrivate(new PKCS8EncodedKeySpec(keyBytes));
+            LOGGER.debug("private key set");
+            keyBytes = Base64.getDecoder().decode(secrets.get("publicKey"));
+            this.publicKey = keyFactory.generatePublic(new X509EncodedKeySpec(keyBytes));
+            LOGGER.debug("public key set");
+            this.password = secrets.get("nodePassword");
+            LOGGER.debug("password set {}", this.password != null);
+        }
+    }
+
+    private static KeyStore.Entry loadKey(String alias, String keystoreFilename, String keystoreType,
+            char[] keystorePass, char[] keyPass) {
+        File f = new File(keystoreFilename);
+        if (!f.exists() || !f.isFile() || !f.canRead()) {
+            throw new IllegalStateException(f.getAbsolutePath() + " is not a regular readable file");
+        }
+        try (InputStream is = new FileInputStream(f)) {
+            KeyStore keyStore = KeyStore.getInstance(keystoreType);
+            keyStore.load(is, keystorePass);
+            ProtectionParameter protector = null;
+            if (keyPass != null) {
+                protector = new KeyStore.PasswordProtection(keyPass);
+            }
+            return keyStore.getEntry(alias, protector);
+        } catch (Exception e) {
+            LOGGER.warn("Error while doing loadKeyStore", e);
+            throw Throwables.propagate(e);
+        }
     }
 
     /**
