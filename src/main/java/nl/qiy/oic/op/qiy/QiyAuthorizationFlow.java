743910689a88358a540f02fd79238d96a67e0c7c
fvrolijken <friso.vrolijken@qiy.nl>
2018-11-01 15:30:14 +0100

    oops :-$

--- src/main/java/nl/qiy/oic/op/qiy/QiyAuthorizationFlow.java
+++ src/main/java/nl/qiy/oic/op/qiy/QiyAuthorizationFlow.java
@@ -366,12 +366,12 @@ public class QiyAuthorizationFlow implements AuthorizationFlow {
      */
     public static QiyAuthorizationFlow getInstance(URL baseDappreURL) {
         if (instance == null) {
+            scheduledThreadPool = Executors.newScheduledThreadPool(1);
             // might take too long for the deployment script, so don't wait for it
             scheduledThreadPool.schedule(() -> QiyNodeClient.readCardMessage(baseDappreURL), 1, TimeUnit.MILLISECONDS);
             instance = new QiyAuthorizationFlow();
             eventStreams = ServerSentEventStreams.getInstance();
 
-            scheduledThreadPool = Executors.newScheduledThreadPool(1);
             // refresh every 12 hours
             scheduledThreadPool.scheduleAtFixedRate(() -> QiyNodeClient.readCardMessage(baseDappreURL), 12, 12,
                     TimeUnit.HOURS);
